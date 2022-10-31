30c24668eea190638edd98c5b1afa8a988977039
Adam Kaplan <adam.kaplan@legitscript.com>
2022-10-31 13:57:49 -0700

    basic instructions.

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,22 @@
-import { useParams } from "react-router";
-
-import JSONDisplay from "../components/JSONDisplay";
-
+import { Link } from "react-router-dom";
 const Home = () => {
-  const params = useParams();
   return (
     <section>
       <h1>Home Page</h1>
-      <JSONDisplay>{JSON.stringify(params, null, 2)}</JSONDisplay>
+      <p>
+        This is a simple implementation of an OIDC client for testing purposes.
+      </p>
+      <p>
+        Use the <Link to="/config">Config Page</Link> to configure an Identity
+        Provider and Client.
+      </p>
+      <p>
+        Once configuration is complete, use the{" "}
+        <Link to="/test">Test Page</Link> to start an Implicit Flow.
+      </p>
+      <p>
+        Upon successful login, you will be shown the contents of the response.
+      </p>
     </section>
   );
 };
