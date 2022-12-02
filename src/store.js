01da05dd3e40b6bc9967fe893a8095d1c96b8a2e
Adam Kaplan <adam.kaplan@legitscript.com>
2022-12-02 10:59:15 -0800

    rotate client id

--- src/store.js
+++ src/store.js
@@ -30,7 +30,7 @@ export const useConfigStore = create(
       },
     })),
     {
-      version: 3,
+      version: 4,
     }
   )
 );
@@ -47,7 +47,7 @@ export const useSessionStore = create(
     })),
     {
       getStorage: () => sessionStorage,
-      version: 3,
+      version: 4,
     }
   )
 );
